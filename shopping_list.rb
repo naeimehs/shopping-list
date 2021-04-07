b9b255054522f919c6d762d9267ef3532710ae05
naeemeh <naeimehs@nulogy.com>
2021-04-07 16:48:30 -0400

    Create ShoppingList and REcipe classes

--- shopping_list.rb
+++ shopping_list.rb
@@ -1,2 +1,23 @@
-puts "Here's the recipe for breakfast: Scrambled eggs"
-puts "Here's your shopping list: 12 eggs, ground coffee"
+
+class ShoppingList
+  attr_reader :recipe_name
+
+  def initialize(recipe_name)
+    @recipe_name = recipe_name
+  end
+
+  def get_shopping_list
+    puts "Here's the recipe for breakfast: #{recipe_name}"
+    print "Here's your shopping list: "
+    puts Recipe.get_ingredients.join(', ')
+  end
+end
+
+class Recipe
+  def self.get_ingredients
+    ["12 eggs", "ground coffee"]
+  end
+end
+
+shopping_list = ShoppingList.new("scrambled eggs")
+shopping_list.get_shopping_list
